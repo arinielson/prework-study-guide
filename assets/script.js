4e7bbf9a4cd3f47cbac530bbf641f445422ed25f
Ari N <an-nielson@hotmail.com>
2022-09-05 13:25:19 -0600

    updated project README file

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
 
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
@@ -27,34 +26,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
-||||||| 69a4beb
-=======
-var topics = ["HTML", "CSS", "Git", "JavaScript"];
-
-var randomTopic = topics[Math.floor(Math.random() * topics.length)];
-
-function listTopics() {
-    for (var x = 0; x < topics.length; x++) {
-      console.log(topics[x]);
-    }
-}
-
-function selectTopic() {
-    if (randomTopic === 'HTML') {
-      console.log("Let's study HTML!");
-    } else if (randomTopic === 'CSS') {
-      console.log("Let's study CSS!");
-    } else if (randomTopic === 'Git') {
-      console.log("Let's study Git!");
-    } else if (randomTopic === 'JavaScript') {
-      console.log("Let's study JavaScript!");
-    } else {
-      console.log('Please try again!');
-    }
-  }
-
-console.log('Here are the topics we learned through Prework:');
-listTopics();
-console.log('Which topic should we study first?');
-selectTopic();
->>>>>>> 0a7d878ef07e6278257cde0d0f09c94f6114741f
